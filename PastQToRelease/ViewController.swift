f243112ca6817e44d0177d8bfdda1131433b9444
owner <owner@yanasanno-macbook-pro.local>
2016-08-01 21:56:43 +0900

    Initial Commit

---
+++ PastQToRelease/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  PastQToRelease
+//
+//  Created by owner on 2016/08/01.
+//  Copyright © 2016年 Yuki Yanagawa. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
